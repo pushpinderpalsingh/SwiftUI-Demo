56463487d900a70042051fa5ff0c69224d82d7df
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-03-29 04:24:39 +0530

    Initial Commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Pushpinder Pal Singh on 29/03/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
